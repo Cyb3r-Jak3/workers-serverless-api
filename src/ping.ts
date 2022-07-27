51440321eebe110ae60bdf7c3dba5ebeb2222d3b
Jacob White <jake@jwhite.network>
2022-07-27 17:40:16 -0400

    Add ping endpoint

---
+++ src/ping.ts
@@ -0,0 +1,13 @@
+import { Context } from "hono";
+import { JSONResponse } from "./utils";
+
+
+export async function PingEndpoint(c: Context): Promise<Response> {
+    var gitHash = c.env.GitHash
+    if (!gitHash) {
+        gitHash = "dev"
+    }
+    const CF_headers = JSON.parse(JSON.stringify(c.req.cf))
+    CF_headers["gitHash"] = gitHash
+    return JSONResponse(CF_headers)
+}
