0b091dcd87b192c903c0187a15bd8b3689859fb7
Jacob White <jake@jwhite.network>
2022-08-01 21:13:18 -0400

    Fix lint

--- src/git.ts
+++ src/git.ts
@@ -8,7 +8,6 @@ const GithubUsername = 'Cyb3r-Jak3'
 const PublicEmail = 'git@cyberjake.xyz'
 const cache = caches.default
 
-
 export async function GithubRepos(c: Context): Promise<Response> {
     let resp = await cache.match(c.req)
     if (resp) {
