ca69f7585bd4d70e919f5523bd695901f95fab2f
Cyb3r-Jak3 <git@Cyberjake.xyz>
2023-04-13 20:53:38 -0400

    Better way to return headers

--- src/misc.ts
+++ src/misc.ts
@@ -3,7 +3,7 @@ import { JSONResponse, JSONAPIResponse } from '@cyb3r-jak3/workers-common'
 
 export async function CFEndpoint(c: Context): Promise<Response> {
     return JSONResponse({
-        headers: c.req.headers,
+        headers: Object.fromEntries(c.req.raw.headers.entries()),
         cf: c.req.raw.cf,
     })
 }
