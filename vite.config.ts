09874a141d1e35db1dd46093a6f9fb69a59c5648
Cyb3r-Jak3 <git@Cyberjake.xyz>
2024-02-10 11:40:49 -0500

    Try vitest pools

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import {defineConfig} from 'vitest/config'
+import { defineWorkersPoolOptions, } from "@cloudflare/vitest-pool-workers/config";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    test: {
-        environment: "miniflare",
-        coverage: {
-            // ToDo: Figure out how to collect coverage
-            enabled: false,
-        }
-    }
-})
+  test: {
+    pool: "@cloudflare/vitest-pool-workers",
+    poolOptions: {
+      workers: defineWorkersPoolOptions({
+        isolatedStorage: true,
+        main: "./src/index.ts", // TypeScript!
+        miniflare: {
+          kvNamespaces: ["KV"],
+        },
+      }),
+    },
+  },
+});
